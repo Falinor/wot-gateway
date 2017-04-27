64e2e9028c9bf850546647b2a7248f8d96f348c3
Andrea Gueugnaut <andrea.gueugnaut@gmail.com>
2017-04-27 18:26:41 +0200

    feat(api): add pi controller methods

--- src/api/pi/controller.js
+++ src/api/pi/controller.js
@@ -1,5 +1,11 @@
 import pi from './model';
 
-export const show = (req, res, next) => {
+export const show = async (req, res, next) => {
+  const model = await pi.model();
+  res.status(200).json(model);
+};
+
+export const showProperties = async (req, res, next) => {
+  const props = await pi.properties();
   // TODO
 };
