c3c91bd09ab7a8cf342066aa7e4ce9e4edd9b916
Andrea Gueugnaut <andrea.gueugnaut@gmail.com>
2017-04-27 11:55:43 +0200

    test(setup): fix test setup

--- test/setup.js
+++ test/setup.js
@@ -1,41 +1,42 @@
+import test from 'ava';
 import { EventEmitter } from 'events'
 import mockgoose from 'mockgoose'
+
 import mongoose from '../src/services/mongoose'
-import { mongo } from '../src/config'
+import config from '../src/config'
 
-EventEmitter.defaultMaxListeners = Infinity
-jasmine.DEFAULT_TIMEOUT_INTERVAL = 10000
+EventEmitter.defaultMaxListeners = Infinity;
 
-global.Array = Array
-global.Date = Date
-global.Function = Function
-global.Math = Math
-global.Number = Number
-global.Object = Object
-global.RegExp = RegExp
-global.String = String
-global.Uint8Array = Uint8Array
-global.WeakMap = WeakMap
-global.Set = Set
-global.Error = Error
-global.TypeError = TypeError
-global.parseInt = parseInt
-global.parseFloat = parseFloat
+global.Array = Array;
+global.Date = Date;
+global.Function = Function;
+global.Math = Math;
+global.Number = Number;
+global.Object = Object;
+global.RegExp = RegExp;
+global.String = String;
+global.Uint8Array = Uint8Array;
+global.WeakMap = WeakMap;
+global.Set = Set;
+global.Error = Error;
+global.TypeError = TypeError;
+global.parseInt = parseInt;
+global.parseFloat = parseFloat;
 
-beforeAll(async () => {
-  await mockgoose(mongoose)
-  mongoose.connect(mongo.uri)
-})
+test.before('Global setup', async () => {
+  await mockgoose(mongoose);
+  mongoose.connect(config.mongo.uri);
+});
 
-afterAll(() => {
+test.after.always('Global cleanup', () => {
   mongoose.disconnect()
-})
+});
 
-afterEach(async () => {
-  const { collections } = mongoose.connection
-  const promises = []
+test.afterEach.always(async () => {
+  const { collections } = mongoose.connection;
+  const promises = [];
   Object.keys(collections).forEach((collection) => {
     promises.push(collections[collection].remove())
-  })
-  await Promise.all(promises)
-})
+  });
+  await Promise.all(promises);
+});
