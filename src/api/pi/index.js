63d84103539a13deed262f4ff50dd957d0dc45f6
Andrea Gueugnaut <andrea.gueugnaut@gmail.com>
2017-04-28 11:31:44 +0200

    feat(api): add /properties/:id route

--- src/api/pi/index.js
+++ src/api/pi/index.js
@@ -1,10 +1,23 @@
 import { Router } from 'express';
+// import { middleware as body } from 'bodymen';
 
-import { show, showProperties } from './controller';
+import { show, showProperties, showProperty } from './controller';
 
 const router = new Router();
 
+/**
+ * Displays Pi's model.
+ */
 router.get('/', show);
+
+/**
+ * Returns Pi's properties.
+ */
 router.get('/properties', showProperties);
+
+/**
+ * Returns a Pi's single property.
+ */
+router.get('/properties/:id', showProperty);
 
 export default router;
